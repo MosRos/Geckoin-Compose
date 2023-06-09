acd8ca3268a27859e343a3fe7b166186b3fe355a
mrostami <moslemrostami@gmail.com>
2023-06-10 01:58:29 +0330

    * progressbar for searching added \n * search view model modified and debunce for search query implemented

--- app/src/main/java/com/mrostami/geckoincompose/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/mrostami/geckoincompose/ui/search/SearchViewModel.kt
@@ -7,7 +7,17 @@ import androidx.paging.cachedIn
 import com.mrostami.geckoincompose.domain.usecases.SearchCoinsUseCase
 import com.mrostami.geckoincompose.model.Coin
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,5 +25,27 @@ class SearchViewModel @Inject constructor(
     private val searchCoinsUseCase: SearchCoinsUseCase
 ) : ViewModel() {
 
-    fun searchCoin(query: String = ""): Flow<PagingData<Coin>> = searchCoinsUseCase.invoke(query).cachedIn(viewModelScope)
+    var uiState: MutableStateFlow<PagingData<Coin>> = MutableStateFlow(PagingData.empty())
+        private set
+//    fun searchCoin(query: String = "") = searchCoinsUseCase.invoke(query).cachedIn(viewModelScope)
+
+    private val searchJob = SupervisorJob()
+    private val searchScope = viewModelScope.plus(searchJob)
+    private val SEARCH_DEBUNCE = 650L
+
+    init {
+        searchCoins(null)
+    }
+    fun searchCoins(searchInput: String?) {
+        searchScope.coroutineContext.cancelChildren()
+        searchScope.launch {
+            delay(SEARCH_DEBUNCE)
+            val result = searchCoinsUseCase.invoke(
+                query = searchInput
+            ).catch { t ->
+
+            }.cachedIn(viewModelScope)
+            uiState.emitAll(result)
+        }
+    }
 }
